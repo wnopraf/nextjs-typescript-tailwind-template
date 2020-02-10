b35fc77dbdde184f60092f8da12d218bbb832f16
wnpraf <rapribu@gmail.com>
2020-02-10 21:01:09 +0100

    Add suggested eslint fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import App from 'next/app'
 import '../public/css/tailwind.css'
 
 class MyApp extends App {
-  render() {
+  render(): ReactElement {
     const { Component, pageProps } = this.props
     return <Component {...pageProps} />
   }
